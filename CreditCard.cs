36aeaa11b218cf140c0a032372cd9b2a4b382522
PASHA011 <abab101083@gmail.com>
2022-07-22 22:57:29 +0300

    Add project files.

---
+++ CreditCard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+   public class CreditCard
+    {
+        public CreditCard(string cardNumber,DateTime expireDate,int cvc)
+        {
+            DateTime ToDaydate = new DateTime(DateTime.Now.Year);
+            
+            int result = DateTime.Compare(expireDate, ToDaydate);
+
+            if (cardNumber.Length!=16)
+            {
+                Console.WriteLine("kart numaranız 16 karater olmalı");
+                return;
+            }
+            else if (cvc > 999||cvc <0)
+            {
+                Console.WriteLine("cvc numarası 3 numaradan oluşmaktadır");
+                return;
+            }
+
+            else if (result>0)
+            {
+                Console.WriteLine("son kullanma tarihi geçmiş ");
+                return;
+            }
+          
+         
+
+            CardNumber = cardNumber;
+            Cvc = cvc;
+            ExpireDate = expireDate;
+        }
+
+        public string CardNumber { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int Cvc { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string inputCardNumber="";
+            DateTime  inputExpireDate=new DateTime() ;
+            int inputCvc=132;
+
+            try
+            {
+                Console.Write("kredi kartınızın numarasını giriniz : ");
+                inputCardNumber = Console.ReadLine();
+                Console.Write("kredi kartınızın Cvc numarasını giriniz : ");
+                inputCvc = Convert.ToInt32(Console.ReadLine());
+                Console.Write("kredi kartınızın son kullanma tarihi giriniz : ");
+                inputExpireDate = Convert.ToDateTime(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Exception CreditCardException = new Exception("Kredi bilgileri yalnış girildi");          
+            }
+          
+            CreditCard creditCard = new CreditCard(
+              inputCardNumber,
+              inputExpireDate,
+              inputCvc);
+        }
+
+
+
+    }
+}
